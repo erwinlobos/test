bccefcbbcdd1ed25de4b2806125d44a855f425f9
Erwin Lobos <erwinlobos@yahoo.com>
2020-01-10 10:42:26 -0600

    5k usuarios

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -22,6 +22,6 @@ class BasicSimulation extends Simulation {
     )
 
   setUp(
-    scn.inject(rampUsers(1000) during (60 seconds))
+    scn.inject(rampUsers(5000) during (60 seconds))
   ).protocols(httpConf)
 }
